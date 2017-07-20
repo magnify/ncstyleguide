8a965ea6a87bb842c21885c0cf5e9eb1495fd075
Brian Stefan Jensen <bsj@novicell.dk>
2017-07-20 15:57:21 +0200

    Added public

---
+++ public/js/novicell.visible.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * @name Novicell Visible
+ * @desc A script that checks whether elements are within the users visible viewport.
+ * @author Brian Stefan Jensen (BSJ)
+ * @example
+ * @requires none
+ */
+
+
+var novicell = novicell || {};
+
+novicell.visible = novicell.visible || new function () {
+  this.isVisible = function(element) {
+    var rect = document.querySelector(element).getBoundingClientRect();
+    var html = document.documentElement;
+
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || html.clientHeight) &&
+      rect.right <= (window.innerWidth || html.clientWidth)
+    );
+  }
+}();
